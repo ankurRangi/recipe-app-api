efd36bcac1d57c72f949cc9aeb7b6f3b5aaf480c
Ankur <ankur.rangi08@gmail.com>
2022-07-26 17:47:59 +0530

    Wating for database to start

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,10 @@
 Calculator Function
 """
 
-def add(x,y):
+
+def add(x, y):
     return x+y
 
-def sub(x,y):
+
+def sub(x, y):
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -18,5 +18,6 @@ class CalcTests(SimpleTestCase):
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
-        res = calc.sub(10,15)
-        self.assertEqual(res,5)
+        res = calc.sub(10, 15)
+
+        self.assertEqual(res, 5)
