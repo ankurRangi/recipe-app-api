43a8f65dfaa5b8f4059eac43c3858e24a37d43db
Ankur <ankur.rangi08@gmail.com>
2022-08-11 12:24:25 +0530

    Added New apps - User & Recipe

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@
 Database Models
 """
 
+from django.conf import settings
+
 # from urllib.request import AbstractBasicAuthHandler
 from django.db import models
 from django.contrib.auth.models import (
@@ -45,3 +47,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
